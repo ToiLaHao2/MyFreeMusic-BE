d956459ac819578e17c27c619bc574ce3508f610
Nguyễn Hữu Hào <haonh@innotech-vn.com>
2026-01-05 23:00:11 +0700

    update structure

--- server-music/config/auth.config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET || "dev-secret-key",
-    JWT_EXPIRES_IN: "7d",
-    REFRESH_EXPIRES_IN: "30d",
-};
--- server-music/config/index.js
+++
@@ -1,9 +0,0 @@
-const db = require("./db.config");
-const auth = require("./auth.config");
-const cloudinary = require("./cloudinary.config");
-
-module.exports = {
-    db,
-    auth,
-    cloudinary,
-};
